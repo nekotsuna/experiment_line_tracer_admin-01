94f3e06fd7bf67062216310b43f1dcd30cf54b7b
3969okki <137705196+3969okki@users.noreply.github.com>
2025-05-20 09:23:22 +0900

    Update course_m.c

--- course_m.c
+++ course_m.c
@@ -8,17 +8,17 @@ void main(){
   PFD pfd;
   int output[5];
   pfd = init();
-  motor_drive(pfd, 8,0);
+  //motor_drive(pfd, 8,0);
 
   while(1){
     get_sensor(pfd, output);
     if(output[1] == 1){
-      moter_drive(pfd, 2, 8);
+      moter_drive(pfd, 4, 8);
     }
-    if(output[3] == 1){
-      moter_drive(pfd, 8, 2);
+    else if(output[3] == 1){
+      moter_drive(pfd, 8, 4);
     }
-    if(output[4] == 1){
+    else if(output[4] == 1){
       while(1){
         motor_drive(pfd, 8, 0);
         if(output[3] == 1){
@@ -26,6 +26,14 @@ void main(){
         }
       }
     }
-    time_sleep(0.01);
+    else if(output[0] == 1){
+      while(1){
+        motor_drive(pfd, 0, 8);
+        if(output[1] == 1){
+          break;
+        }
+      }
+    }
+    time_sleep(0.1);
   }
 }
