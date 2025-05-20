5d423d856bc6d83101a8b2186e298fb307714cbc
KI-58 <itto5884@gmail.com>
2025-05-20 09:20:14 +0900

    Update course_q.c

--- course_q.c
+++ course_q.c
@@ -1,1 +1,18 @@
-a
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <pigpiod_if2.h>
+
+#include "common.c"
+
+void main() {
+    PFD pfd;
+    int output[5];
+    pfd = init();
+
+    motor_drive(pfd, 0, 0);
+
+    while(1){
+      
+   }
+	
+}
