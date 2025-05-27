6f65c5be96184ac726c1767984253d8173eaff13
aregurakun <161214536+aregurakun@users.noreply.github.com>
2025-05-27 09:02:51 +0900

    Update course_n.c

--- course_n.c
+++ course_n.c
@@ -1,1 +1,41 @@
-aaaaa
+#include<stdio.h>
+#include<stdlib.h>
+#include<pigpiod_if2.h>
+
+#include "common.c"
+
+void main(){
+  PFD pfd;
+  int output[5];
+  pfd = init();
+  motor_drive(pfd, 0, 0);
+
+  while(1){
+    get_sensor(pfd, output);
+
+    if(output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 0 && output[4] == 0){
+	motor_drive(pfd, 8, 8);
+
+    if(output[0] == 1 && output[1] == 1 && output[3] == 0){
+	motor_drive(pfd, 8, 5);
+    }
+    else if(output[3] == 1 && output[4] == 1 && output[1] == 0){
+	motor_drive(pfd, 5, 8);
+    }
+
+    if(output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 1 && output[4] == 1){
+	motor_drive(pfd, 8, 0);
+	if(output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 0 && output[4] == 1){
+		motor_drive(pfd, 8, 0);
+		if(output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 1 && output[4] == 0){
+			motor_drive(pfd, 8, 0);
+			if(output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 0 && output[4] == 0){
+				motor_drive(pfd, 8, 8);
+			}
+		}
+	}
+    }
+    time_sleep(0.01);
+  }
+}
+
