3ccf604ff2627e292851d368dae697bd6f6cf46e
nekotsuna <rffkminato@yahoo.co.jp>
2025-05-27 10:10:28 +0900

    update course_m

--- course_m.c
+++ course_m.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<pigpiod.h>
+#include<pigpiod_if2.h>
 
 #include "common.c"
 
@@ -13,29 +13,43 @@ void main(){
   while(1){
     get_sensor(pfd, output);
     if(output[1] == 1){
-      moter_drive(pfd, 4, 8);
+      motor_drive(pfd, 3, 5);
+		printf("straight\n");
     }
     else if(output[3] == 1){
-      moter_drive(pfd, 8, 4);
+      motor_drive(pfd, 5, 3);
+		printf("straight\n");
     }
     else if(output[4] == 1){
-      time_sleep(0.5);
+      motor_drive(pfd, 12, 0);
+      time_sleep(1.0);
       while(1){
-        motor_drive(pfd, 8, -4);
+	get_sensor(pfd, output);
+        motor_drive(pfd,12, 0);
+	printf("turn_left\n");
         if(output[3] == 1){
+          printf("turn_left_end\n");
           break;
         }
+	time_sleep(0.1);
       }
     }
     else if(output[0] == 1){
-      time_sleep(0.5)
+      motor_drive(pfd, 0, 12);
+      time_sleep(1.0);
       while(1){
-        
-        motor_drive(pfd, -4, 8);
+        get_sensor(pfd, output);
+        motor_drive(pfd, 0, 12);
+        printf("turn_right\n");
         if(output[1] == 1){
+          printf("turn_right_en\n");
           break;
         }
+	time_sleep(0.1);
       }
+    }
+    else{
+      motor_drive(pfd, 8, 8);
     }
     time_sleep(0.1);
   }
