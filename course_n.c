455a99a669fc41dd431b5a83649151ae76b9a35b
aregurakun <161214536+aregurakun@users.noreply.github.com>
2025-06-03 09:42:35 +0900

    Update course_n.c

--- course_n.c
+++ course_n.c
@@ -17,23 +17,29 @@ void main(){
 	motor_drive(pfd, 10, 10);
     }
 	  
-    if(output[0] == 1 && output[1] == 1 && output[3] == 0){
-	motor_drive(pfd, 9, 6);
-    }
-    
-    else if(output[3] == 1 && output[4] == 1 && output[1] == 0){
-	motor_drive(pfd, 6, 9);
+    if(output[0] == 1){
+	motor_drive(pfd, 10, 7);
     }
 
-    if(output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 1 && output[4] == 1){
+    else if(output[1] == 1){
+	 motor_drive(pfd, 9, 6);
+    }
+
+    else if(output[3] == 1){
+	 motor_drive(pfd, 6, 9);
+    }
+    
+    else if(output[4] == 1){
+	motor_drive(pfd, 7, 10);
+    }
+
+    if(output[3] == 1 && output[4] == 1){
 	motor_drive(pfd, 10, 0);
-	if(output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 0 && output[4] == 1){
-    	motor_drive(pfd, 10, 0);
 		if(output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 0 && output[4] == 0){
 			motor_drive(pfd, 10, 10);
 		}
-	}
     }
     time_sleep(0.01);
   }
 }
+
