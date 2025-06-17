bdd1e3eb8072379bd2dec13939adbafe30bb8b44
aregurakun <161214536+aregurakun@users.noreply.github.com>
2025-06-17 09:37:11 +0900

    Update course_n.c

--- course_n.c
+++ course_n.c
@@ -9,41 +9,45 @@ void main(){
   int output[5];
   pfd = init();
   motor_drive(pfd, 0, 0);
+  int flag = 1;
 
   while(1){
     get_sensor(pfd, output);
 
-    if(output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 0 && output[4] == 0){
-	motor_drive(pfd, 10, 10);
+    if((output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 0 && output[4] == 0) || (output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 0 && output[4] == 0)){
+        flag = 1;//直進
     }
 
-    else if(output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 0 && output[4] == 0){
-	    motor_drive(pfd, 10, 10);
-    }
 	  
     if(output[0] == 1){
-	motor_drive(pfd, 10, 7);
+        flag = 2;//右に方向調整
     }
 
-    else if(output[1] == 1){
-	 motor_drive(pfd, 9, 6);
-    }
-
-    else if(output[3] == 1){
-	 motor_drive(pfd, 6, 9);
-    }
-    
     else if(output[4] == 1){
-	motor_drive(pfd, 7, 10);
+        flag = 3;//左に方向調整
     }
 
     if(output[3] == 1 && output[4] == 1){
-	motor_drive(pfd, 10, 0);
-		if(output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 0 && output[4] == 0){
-			motor_drive(pfd, 10, 10);
-		}
+	flag = 4;//全力の右折
     }
+
+
+    if(flag == 1){
+	motor_drive(pfd, 10, 10);
+    }
+  
+    else if(flag == 2){
+	motor_drive(pfd, 10, 7);
+    }
+
+    else if(flag == 3){
+	motor_drive(pfd, 7, 10);
+    }
+
+　　else if(flag == 4){
+	motor_drive(pfd, 10, 0);
+    }
+
     time_sleep(0.01);
   }
 }
-
