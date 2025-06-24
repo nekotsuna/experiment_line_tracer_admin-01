1de30707cbb139ceb750ed02ba64f93af7604436
aregurakun <161214536+aregurakun@users.noreply.github.com>
2025-06-24 10:15:58 +0900

    Update course_n.c

--- course_n.c
+++ course_n.c
@@ -1,67 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+ 
 #ifdef IN_RASPBERRYPI
-
+ 
 #include <pigpiod_if2.h>
 #include "common.c"
-
+ 
 #else
-
+ 
 #include "dummy_common.c"
-
+ 
 #endif
-
+ 
 void main(){
   PFD pfd;
   int output[5];
   pfd = init();
   motor_drive(pfd, 0, 0);
   int flag = 1;
-
   while(1){
     get_sensor(pfd, output);
-
+ 
     if((output[0] == 0 && output[1] == 0 && output[2] == 0 && output[3] == 0 && output[4] == 0) || (output[0] == 0 && output[1] == 0 && output[2] == 1 && output[3] == 0 && output[4] == 0)){
         flag = 1;//直進
     }
-
-    if(output[3] == 1 && output[4] == 1){
+ 
+    else if(output[3] == 1 && output[4] == 1){
         flag = 2;//全力の右折
     }
-  
-    if(output[0] == 1 && output[1] == 0){
-        flag = 3;//右に方向調整
+    else if(output[0] == 1){
+        flag = 3;//左に方向調整
     }
-
-    if(output[3] == 0 && output[4] == 1){
-        flag = 4;//左に方向調整
+ 
+    else if(output[4] == 1){
+        flag = 4;//右に方向調整
     }
-
-    
-    
-    
-
+ 
+    else{
+          flag = 1;
+    }
+ 
+ 
     if(flag == 2){
         printf("2\n");
         motor_drive(pfd, 10, 0);
     }
-
-    if(flag == 1){
+ 
+    else if(flag == 1){
         printf("1\n");
         motor_drive(pfd, 10, 10);
     }
-  
     else if(flag == 3){
         printf("3\n");
         motor_drive(pfd, 7, 5);
     }
-
+ 
     else if(flag == 4){
         printf("4\n");
         motor_drive(pfd, 5, 7);
     }
-
+ 
     time_sleep(0.01);
   }
 }
