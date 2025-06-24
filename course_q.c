cacbde9645864ff41c3513a34a1d327255198f60
KI-58 <itto5884@gmail.com>
2025-06-24 09:16:38 +0900

    Update course_q.c

--- course_q.c
+++ course_q.c
@@ -1,8 +1,14 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <pigpiod_if2.h>
 
+#ifdef IN_RASPBERRYPI
+
+#include <pigpiod_if2.h>
 #include "common.c"
+
+#else
+
+#include "dummy_common.c"
 
 void main() {
     PFD pfd;
