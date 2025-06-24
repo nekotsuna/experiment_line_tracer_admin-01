4ae5b4e8dc578b014739a287bd684f19d75353f1
3969okki <137705196+3969okki@users.noreply.github.com>
2025-06-24 09:45:22 +0900

    Update course_m.c

--- course_m.c
+++ course_m.c
@@ -19,15 +19,15 @@ void main() {
                 get_sensor(pfd, output);
 
                 if (output[1] == 1) {
-                    motor_drive(pfd, 3, 5);
+                    motor_drive(pfd, 2, 4);
                 } else if (output[0] == 1) {
-                    motor_drive(pfd, 1, 5);
+                    motor_drive(pfd, 1, 4);
                 } else if (output[3] == 1) {
-                    motor_drive(pfd, 5, 3);
+                    motor_drive(pfd, 4, 2);
                 } else if (output[4] == 1) {
-                    motor_drive(pfd, 5, 1);
+                    motor_drive(pfd, 4, 1);
                 } else {
-                    motor_drive(pfd, 8, 8);
+                    motor_drive(pfd, 4, 4);
                 }
 
                 if ((output[0] == 1 && output[2] == 1) ||
@@ -68,7 +68,7 @@ void main() {
                         break;
                     }
                 } else {
-                    motor_drive(pfd, 2, 2);
+                    motor_drive(pfd, 3, 3);
                     countdown = 0;
                 }
 
