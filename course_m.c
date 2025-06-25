055289077bcbe3998632f33e8f7e3e110b77008e
3969okki <137705196+3969okki@users.noreply.github.com>
2025-06-25 15:53:06 +0900

    Update course_m.c

--- course_m.c
+++ course_m.c
@@ -84,7 +84,7 @@ void main() {
             */
             for(int i =0; i < 12; i++){
             printf("2");
-            motor_drive(pfd, -5, 10);
+            motor_drive(pfd, 4, 12);
             time_sleep(0.05);
             }
             MODE = 1;
