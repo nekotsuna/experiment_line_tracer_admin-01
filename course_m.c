04a03c425277fadeabebcf4fc11a1c40eb1449fa
3969okki <137705196+3969okki@users.noreply.github.com>
2025-06-25 16:35:14 +0900

    Update course_m.c

--- course_m.c
+++ course_m.c
@@ -19,16 +19,16 @@ void main() {
                 printf("1");
                 get_sensor(pfd, output);
 
-                if (output[1] == 1) {
-                    motor_drive(pfd, 4, 8);
-                } else if (output[0] == 1) {
-                    motor_drive(pfd, 1, 11);
-                } else if (output[2] == 1) {
-                    motor_drive(pfd, 6, 6);
+                if (output[4] == 1) {
+                    motor_drive(pfd, 11, 1);
                 } else if (output[3] == 1) {
                     motor_drive(pfd, 8, 4);
-                } else if(output[4] == 1){
-                    motor_drive(pfd, 11, 1);
+                } else if (output[2] == 1) {
+                    motor_drive(pfd, 6, 6);
+                } else if (output[1] == 1) {
+                    motor_drive(pfd, 4, 8);
+                } else if(output[0] == 1){
+                    motor_drive(pfd, 1, 11);
                 }
                 /*
                 if ((output[0] == 1 && output[2] == 1) ||
