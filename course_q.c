562439e42762cb7834ef4ad00837f8f609ad80dd
KI-58 <itto5884@gmail.com>
2025-07-08 09:46:53 +0900

    Update course_q.c

--- course_q.c
+++ course_q.c
@@ -34,7 +34,7 @@ void main() {
       motor_drive(pfd,9,9);
 	
       if(output[4] == 1){
-	motor_drive(pfd,12,5);
+	motor_drive(pfd,11,5);
       }
       else if(output[3] == 1){
 	motor_drive(pfd,10,5);
