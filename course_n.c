490392da1b06aeccbd81e93ce90997a26e213db7
aregurakun <161214536+aregurakun@users.noreply.github.com>
2025-07-14 15:44:27 +0900

    Update course_n.c

--- course_n.c
+++ course_n.c
@@ -68,7 +68,7 @@ void main(){
     }
  
     if(flag == 2){
-        for(int i=0; i<9; i++){
+        for(int i=0; i<8; i++){
 	    get_sensor(pfd, output);
 	    if(output[1] == 0){
 		    if(flag_turn == 0 || flag_turn == 1){
