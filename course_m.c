1f83fc6d073f8fa049ba99c92eb8caa44a59cafc
3969okki <137705196+3969okki@users.noreply.github.com>
2025-07-18 15:30:59 +0900

    Update course_m.c

--- course_m.c
+++ course_m.c
@@ -59,7 +59,7 @@ void main() {
                 } else if(output[0] == 1){
                     motor_drive(pfd, 16, 13);
                 }else{
-                    motor_drive(pfd,-12,12);
+                    motor_drive(pfd,-6,12);
                 }
 
                 /*
